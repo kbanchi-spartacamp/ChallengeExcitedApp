8d5ec44266739548c3c9b6babf3bd1c5a2bfeb20
kbanchi1111 <kbanchi.1111@gmail.com>
2021-11-25 22:30:00 +0900

    update

---
+++ ChallengeExcitedApp/models/AvatorCategory.swift
@@ -0,0 +1,13 @@
+//
+//  AvatorCategory.swift
+//  ChallengeExcitedApp
+//
+//  Created by 伴地慶介 on 2021/11/25.
+//
+
+import Foundation
+
+struct AvatorCategory {
+    let id: Int
+    let name: String
+}
---
+++ ChallengeExcitedApp/models/UserAvator.swift
@@ -0,0 +1,16 @@
+//
+//  UserAvator.swift
+//  ChallengeExcitedApp
+//
+//  Created by 伴地慶介 on 2021/11/25.
+//
+
+import Foundation
+
+struct UserAvator {
+    let id: Int
+    let user_id: Int
+    let avator_category_id: Int
+    let level: Int
+    let avator_category: AvatorCategory
+}
